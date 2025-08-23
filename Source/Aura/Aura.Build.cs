03c037b985b764b1f7ea868410acc6c9dc992444
ugoo0 <1048352685@qq.com>
2025-08-23 16:00:42 +0800

    AuraPlayerControlle and movement input

--- Source/Aura/Aura.Build.cs
+++ Source/Aura/Aura.Build.cs
@@ -7,7 +7,7 @@ public class Aura : ModuleRules
 	public Aura(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
         //bEnableUndefinedIdentifierWarnings = false;
